3cf42a1fe6c1808f766e910ea1b6c1d08f5e5855
Javier Lima <javi.limashelby@gmail.com>
2024-09-23 10:29:47 -0400

    feat: Define EventHandler to start/create agents

--- src/environment.py
+++ src/environment.py
@@ -4,7 +4,7 @@ import random
 from threading import Lock, Thread
 import time
 from uuid import UUID, uuid4
-from ui.globals import Directions, valid_coordinates
+from globals import Directions, valid_coordinates
 
 
 class Block(ABC):
@@ -24,75 +24,25 @@ class RoadBlock(Block):
     def __init__(self, coordinates: tuple[int, int], direction: int) -> None:
         super().__init__(coordinates)
         self.direction = direction
-        self.car: Car = None
+        self.car_id: UUID = None
 
 
 class SidewalkBlock(Block):
     def __init__(self, coordinates: tuple[int, int], vertical: bool) -> None:
         super().__init__(coordinates)
         self.vertical = vertical
-        self.walker: Walker = None
+        self.walker_id: UUID = None
 
 
 class Environment:
     def __init__(self, matrix: list[list[Block]]) -> None:
         self.matrix = matrix
-        self.cars: dict[UUID, Car] = {}
-        self.walkers: dict[UUID, Walker] = {}
+        self.cars: dict[UUID, tuple[int, int]] = {}
+        self.walkers: dict[UUID, tuple[int, int]] = {}
         self.semaphores: dict[tuple[int, int], int] = {}
         self._add_semaphores()
 
         self.lock = Lock()
-
-        #######################################
-        # Setup InitialCars.
-        cars = []
-        height = len(self.matrix)
-        width = len(self.matrix[0])
-
-        for i in range(height):
-            for j in range(width):
-                if isinstance(self.matrix[i][j], RoadBlock) and random.randint(0, 5) == 0:
-                    cars.append(RandomCar((i, j)))
-
-        for car in cars:
-            self.cars[car.id] = car
-
-
-        # Setup initial walkers
-        walkers = []
-        height = len(self.matrix)
-        width = len(self.matrix[0])
-
-        for i in range(height):
-            for j in range(width):
-                if isinstance(self.matrix[i][j], SidewalkBlock) and random.randint(0, 5) == 0:
-                    walkers.append(RandomWalker((i, j)))
-
-        for walker in walkers:
-            self.walkers[walker.id] = walker
-        
-        #######################################
-
-        #######################################
-        # Lines for testing, remove when needed
-        thread = Thread(target=self.random_cars)
-        thread.daemon = True
-        thread.start()
-        thread = Thread(target=self.random_semaphores)
-        thread.daemon = True
-        thread.start()
-        thread = Thread(target=self.random_walkers)
-        thread.daemon = True
-        thread.start()
-        #######################################
-
-    def random_cars(self):
-        while True:
-            time.sleep(0.5)
-            with self.lock:
-                for car_id in self.cars:
-                    self.cars[car_id].move(self)
 
     def _add_semaphores(self) -> None:
         height = len(self.matrix)
@@ -103,90 +53,3 @@ class Environment:
                 block = self.matrix[i][j]
                 if isinstance(block, SemaphoreBlock):
                     self.semaphores[block.representative] = Directions.NORTH
-
-        
-    # Method for testing
-    def random_semaphores(self):
-        while True:
-            time.sleep(0.5)
-            with self.lock:
-                for semaphore_id in self.semaphores:
-                    self.semaphores[semaphore_id] = random.choice(
-                        [
-                            Directions.NORTH,
-                            Directions.SOUTH,
-                            Directions.EAST,
-                            Directions.WEST,
-                        ]
-                    )
-
-    # Method for testing
-    def random_walkers(self):
-        while True:
-            time.sleep(0.5)
-            with self.lock:
-                for walker_id in self.walkers:
-                    self.walkers[walker_id].move(self)
-
-
-class Agent(ABC):
-    def __init__(self, current_pos : tuple[int, int]):
-        self.current_pos = current_pos
-        self.id = uuid4()
-
-class Car(Agent, ABC):
-    @abstractmethod
-    def move(self, environment : Environment):
-        pass
-
-class Walker(Agent, ABC):
-    @abstractmethod
-    def move(self, environment : Environment):
-        pass
-
-class RandomWalker(Walker):
-    def move(self, environment: Environment):
-        i, j = self.current_pos
-        offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        random.shuffle(offsets)
-
-        for m, n in offsets:
-            x = i + m
-            y = j + n
-            if valid_coordinates(x, y, len(environment.matrix), len(environment.matrix[0])): 
-                if isinstance(environment.matrix[x][y], SidewalkBlock):
-                    self.current_pos = (x, y)
-                    return
-                        
-
-class RandomCar(Car):
-    def __init__(self, start_pos : tuple[int, int]):
-        super().__init__(start_pos)
-
-
-    def move(self, environment: Environment):
-        i, j = self.current_pos
-        offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        random.shuffle(offsets)
-
-        for m, n in offsets:
-            x = i + m
-            y = j + n
-            if valid_coordinates(x, y, len(environment.matrix), len(environment.matrix[0])): 
-                if isinstance(environment.matrix[x][y], RoadBlock):
-                    if environment.matrix[i][j].direction == environment.matrix[x][y].direction:
-                        self.current_pos = (x, y)
-                        return
-                if isinstance(environment.matrix[x][y], SemaphoreBlock):
-                    representative = environment.matrix[x][y].representative
-                    direction = environment.matrix[i][j].direction
-                    print(representative, direction)
-                    if direction == environment.semaphores[representative]:
-                        if direction == Directions.NORTH:
-                            self.current_pos = (i, j+2)
-                        if direction == Directions.SOUTH:
-                            self.current_pos = (i, j-2)
-                        if direction == Directions.EAST:
-                            self.current_pos = (i-2, j)
-                        if direction == Directions.WEST:
-                            self.current_pos = (i+2, j)
---
+++ src/sim/event_handler.py
@@ -0,0 +1,60 @@
+import random
+from threading import Thread
+from environment import Environment, RoadBlock, SidewalkBlock
+from sim.agents import Car, Semaphore, Walker
+
+class EventHandler:
+    def __init__(self, environment : Environment) -> None:
+        self.environment = environment
+
+    def start(self):
+        self._start_semaphores()
+        
+        # Create 20 car-events/walker-events
+        for _ in range(20):
+            self._car_event()
+            self._walker_event()
+
+    def _start_semaphores(self):
+        for semaphore_id in self.environment.semaphores:
+            semaphore = Semaphore(semaphore_id, self.environment)
+            semaphore_tread = Thread(target=semaphore.act)
+            semaphore_tread.daemon = True
+            semaphore_tread.start()
+            
+
+    def _car_event(self):
+        with self.environment.lock:
+            free_blocks = self._get_free_blocks(RoadBlock)
+            block = random.choice(free_blocks)
+
+            car = Car(block.coordinates, self.environment)
+            car_thread = Thread(target=car.act)
+            car_thread.daemon = True
+            car_thread.start()
+
+    def _walker_event(self):
+        with self.environment.lock:
+            free_blocks = self._get_free_blocks(SidewalkBlock)
+            block = random.choice(free_blocks)
+
+            walker = Walker(block.coordinates, self.environment)
+            walker_thread = Thread(target=walker.act)
+            walker_thread.daemon = True
+            walker_thread.start()
+        
+    def _get_free_blocks(self, block_type : type):
+        matrix = self.environment.matrix
+        height = len(matrix)
+        width = len(matrix[0])
+        free_blocks : list[RoadBlock] = []
+
+        for i in range(height):
+            for j in range(width):
+                block = matrix[i][j]
+                if isinstance(block, block_type):
+                    if block_type == RoadBlock and block.car_id == None:
+                        free_blocks.append(block)
+                    elif block_type == SidewalkBlock and block.walker_id == None:
+                        free_blocks.append(block)
+        return free_blocks
