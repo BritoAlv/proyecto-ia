6f02d004c6fd9cce11c4961d1db64aef2b105165
Javier Lima <javi.limashelby@gmail.com>
2024-09-25 13:20:32 -0400

    refactor: Introduce a widget-dictionary to improve widget-management

--- src/ui/build_window.py
+++ src/ui/build_window.py
@@ -1,16 +1,35 @@
+from enum import Enum
 import os
 import sys
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QHBoxLayout, QScrollArea, QGridLayout, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QHBoxLayout, QScrollArea, QGridLayout, QVBoxLayout, QPushButton, QLabel, QLineEdit
 import pickle
 
 from environment import Block, RoadBlock, SemaphoreBlock, SidewalkBlock
 from globals import DIRECTION_COLOR, Colors, Directions, valid_coordinates
 from ui.tile import Tile
 
+class _Widgets(Enum):
+    add_road_button = 0
+    remove_road_button = 1
+    add_place_button = 2
+    remove_place_button = 3
+    save_button = 4
+    stop_button = 5
+    name_button = 6
+    enter_name_button = 7
+    enter_place_button = 8
+    zoom_in_button = 9
+    zoom_out_button = 10
+    name_input = 11
+    place_name_input = 12
+    place_description_input = 13
+
+
 class BuildWindow(QMainWindow):
     def __init__(self, height : int = 40, width : int = 40):
         super().__init__()
+        self._map_name : str = ''
         self._grid_height = height
         self._grid_width = width
         self._tiles : list[list[Tile]] = []
@@ -22,35 +41,53 @@ class BuildWindow(QMainWindow):
         self.setMinimumWidth(300)
         self.setMinimumHeight(300)
 
-        main_layout = QHBoxLayout()
+        self._control_widgets : dict[_Widgets, QWidget] = {}
 
-        # **Control layout set up
-        buttons_layout = QVBoxLayout()
-        self.add_road_button = QPushButton("Add Road")
-        self.remove_road_button = QPushButton("Remove Road")
-        self.add_place_button = QPushButton("Add Place")
-        self.remove_place_button = QPushButton("Remove Place")
-        self.save_button = QPushButton("Save")
-        self.stop_button = QPushButton("Stop")
-        self.zoom_in_button = QPushButton("+")
-        self.zoom_out_button = QPushButton("-")
-        self.stop_button.hide()
+        main_layout = QVBoxLayout()
+        self._message_label = QLabel()
+        main_layout.addWidget(self._message_label)
+        
+        core_layout = QHBoxLayout()
 
-        self.add_road_button.clicked.connect(self._handle_add_road)
-        self.remove_road_button.clicked.connect(self._handle_remove_road)
-        self.stop_button.clicked.connect(self._handle_stop)
-        self.zoom_in_button.clicked.connect(self._handle_zoom_in)
-        self.zoom_out_button.clicked.connect(self._handle_zoom_out)
-        self.save_button.clicked.connect(self._handle_save)
+        # Control layout set up
+        ## Buttons
+        control_layout = QVBoxLayout()
+        self._control_widgets[_Widgets.add_road_button] = QPushButton("Add Road")
+        self._control_widgets[_Widgets.remove_road_button] = QPushButton("Remove Road")
+        self._control_widgets[_Widgets.add_place_button] = QPushButton("Add Place")
+        self._control_widgets[_Widgets.remove_place_button] = QPushButton("Remove Place")
+        self._control_widgets[_Widgets.save_button] = QPushButton("Save")
+        self._control_widgets[_Widgets.stop_button] = QPushButton("Stop")
 
-        buttons_layout.addWidget(self.add_road_button)
-        buttons_layout.addWidget(self.remove_road_button)
-        buttons_layout.addWidget(self.add_place_button)
-        buttons_layout.addWidget(self.remove_place_button)
-        buttons_layout.addWidget(self.save_button)
-        buttons_layout.addWidget(self.zoom_in_button)
-        buttons_layout.addWidget(self.zoom_out_button)
-        buttons_layout.addWidget(self.stop_button)
+        self._control_widgets[_Widgets.name_button] = QPushButton('Name')
+        self._control_widgets[_Widgets.enter_name_button] = QPushButton('Enter')
+        self._control_widgets[_Widgets.enter_place_button] = QPushButton('Enter')
+
+        self._control_widgets[_Widgets.zoom_in_button] = QPushButton("+")
+        self._control_widgets[_Widgets.zoom_out_button] = QPushButton("-")
+        
+        ## Text input
+        self._control_widgets[_Widgets.name_input] = QLineEdit()
+        self._control_widgets[_Widgets.place_name_input] = QLineEdit()
+        self._control_widgets[_Widgets.place_description_input] = QLineEdit()
+
+        ## Display required widgets
+        self._show_widgets(self._home_widgets_predicate)
+
+        ## Connect to handle events
+        self._control_widgets[_Widgets.add_road_button].clicked.connect(self._handle_add_road)
+        self._control_widgets[_Widgets.remove_road_button].clicked.connect(self._handle_remove_road)
+        self._control_widgets[_Widgets.stop_button].clicked.connect(self._handle_stop)
+        self._control_widgets[_Widgets.zoom_in_button].clicked.connect(self._handle_zoom_in)
+        self._control_widgets[_Widgets.zoom_out_button].clicked.connect(self._handle_zoom_out)
+        self._control_widgets[_Widgets.save_button].clicked.connect(self._handle_save)
+        self._control_widgets[_Widgets.name_button].clicked.connect(self._handle_name)
+        self._control_widgets[_Widgets.enter_name_button].clicked.connect(self._handle_enter_name)
+        self._control_widgets[_Widgets.enter_place_button].clicked.connect(self._handle_enter_place)
+
+        ## Wire with parent layout
+        for widget in self._control_widgets.values():
+            control_layout.addWidget(widget)
 
         # **Grid layout set up
         scroll_area = QScrollArea()
@@ -73,8 +110,9 @@ class BuildWindow(QMainWindow):
 
         # **Wire up
         scroll_area.setWidget(grid_container)
-        main_layout.addWidget(scroll_area)
-        main_layout.addLayout(buttons_layout)
+        core_layout.addWidget(scroll_area)
+        core_layout.addLayout(control_layout)
+        main_layout.addLayout(core_layout)
 
         widget = QWidget()
         widget.setLayout(main_layout)
@@ -82,14 +120,25 @@ class BuildWindow(QMainWindow):
 
     def _handle_add_road(self):
         self._add_road = True
-        self._hide_buttons()
+        self._show_widgets(lambda widget_id: widget_id in [_Widgets.stop_button])
+
         self._paint_tiles(Colors.YELLOW, lambda i, j: self._matrix[i][j] == 0 and (i == 0 or j == 0 or i == self._grid_width - 1 or j == self._grid_height -1) and i != j and (i, j) != (0, self._grid_width - 1) and (i, j) != (self._grid_height - 1, 0))
 
     def _handle_remove_road(self):
         self._remove_road = True
-        self._hide_buttons()
+        self._show_widgets(lambda widget_id: widget_id in [_Widgets.stop_button])
+
+    def _handle_name(self):
+        self._show_widgets(lambda widget_id: widget_id in [_Widgets.enter_name_button, _Widgets.name_input])
+        self._control_widgets[_Widgets.name_input].setText(self._map_name)
+
 
     def _handle_save(self):
+        if self._map_name == '':
+            self._message_label.setText("You must name the map created")
+            return
+        
+        self._message_label.setText("Saving")
         block_matrix : list[list[Block]] = []
         
         for i in range(self._grid_height):
@@ -122,14 +171,21 @@ class BuildWindow(QMainWindow):
         
         if 'matrices' not in os.listdir('./src/ui/'):
             os.mkdir('./src/ui/matrices')
+
+        # if f'{self._map_name}.pkl' in os.listdir('./src/ui/matrices'):
+        #     self._message_label.setText('There is already a map named like that, please use other name')
+        #     return
         
-        with open("./src/ui/matrices/matrix.pkl", 'wb') as file:
+        with open(f"./src/ui/matrices/{self._map_name}.pkl", 'wb') as file:
             pickle.dump(block_matrix, file)
+        self._message_label.setText("Map successfully saved")
         
     def _handle_stop(self):
         self._add_road = False
         self._remove_road = False
-        self._show_buttons()
+
+        self._show_widgets(self._home_widgets_predicate)
+
         self._paint_tiles(Colors.GREY, lambda i, j: self._matrix[i][j] == 0 and (i == 0 or j == 0 or i == self._grid_width - 1 or j == self._grid_height -1) and i != j and (i, j) != (0, self._grid_width - 1) and (i, j) != (self._grid_height - 1, 0))
 
     def _handle_zoom_in(self):
@@ -141,7 +197,15 @@ class BuildWindow(QMainWindow):
         for i in range(self._grid_height):
             for j in range(self._grid_width):
                 self._tiles[i][j].resize(-4)
-    
+            
+    def _handle_enter_name(self):
+        self._map_name = self._control_widgets[_Widgets.name_input].text()
+        self._show_widgets(self._home_widgets_predicate)
+        print(self._map_name)
+
+    def _handle_enter_place(self):
+        pass
+
     def _handle_tile_clicked(self, coordinates : tuple[int, int]):
         i, j = coordinates
         direction = self._matrix[i][j]
@@ -167,7 +231,7 @@ class BuildWindow(QMainWindow):
                 else:
                     self._tiles[p][q].set_color(color)
                     self._matrix[p][q] = direction
-        elif self.remove_road_button and direction not in [Directions.EMPTY, Directions.INTERSECTION]:
+        elif self._remove_road and direction not in [Directions.EMPTY, Directions.INTERSECTION]:
             vertical = direction in [Directions.NORTH, Directions.SOUTH]
             length = self._grid_height if vertical else self._grid_width
 
@@ -197,31 +261,25 @@ class BuildWindow(QMainWindow):
                         direction = self._matrix[p_prime][q_prime]
                         self._matrix[p][q] = direction
                         self._tiles[p][q].set_color(DIRECTION_COLOR[direction])
-            
 
-    def _show_buttons(self):
-        self.add_road_button.show()
-        self.remove_road_button.show()
-        self.add_place_button.show()
-        self.remove_place_button.show()
-        self.save_button.show()
-        self.stop_button.hide()
+    def _show_widgets(self, predicate = lambda widget_id: True) -> None:
+        for widget_id in self._control_widgets:
+            if predicate(widget_id):
+                self._control_widgets[widget_id].show()
+            else:
+                self._control_widgets[widget_id].hide()
     
-    def _hide_buttons(self):
-        self.add_road_button.hide()
-        self.remove_road_button.hide()
-        self.add_place_button.hide()
-        self.remove_place_button.hide()
-        self.save_button.hide()
-        self.stop_button.show()
-
     def _paint_tiles(self, color, predicate = lambda i, j: True):
         for i in range(self._grid_height):
             for j in range(self._grid_width):
                 if predicate(i, j):
                     self._tiles[i][j].set_color(color)
+
+    def _home_widgets_predicate(self, widget_id):
+        return widget_id not in [_Widgets.stop_button, _Widgets.enter_name_button, _Widgets.enter_place_button, _Widgets.name_input, _Widgets.place_name_input, _Widgets.place_description_input]
         
-app = QApplication(sys.argv)
-window = BuildWindow(20, 20)
-window.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = BuildWindow(20, 20)
+    window.showMaximized()
+    app.exec_()
