37d85737bb916948527b9e3e911bedd85b6cd692
Javier Lima <javi.limashelby@gmail.com>
2024-09-27 09:44:09 -0400

    feat/refactor: Extract places from matrix and comment code

--- src/environment.py
+++ src/environment.py
@@ -1,9 +1,6 @@
 from abc import ABC
 import random
-from threading import Lock
-import time
 from uuid import UUID
-from globals import Directions
 
 
 class Block(ABC):
@@ -41,33 +38,41 @@ class SidewalkBlock(Block):
 
 class Environment:
     def __init__(self, matrix: list[list[Block]]) -> None:
-        self.matrix = matrix
-
+        # Local imports
         from sim.Car import Car
-        self.cars: dict[UUID, Car] = {}
-        
-        from sim.Walker import Walker
-        self.walkers: dict[UUID, Walker] = {}
-
         from sim.Semaphore import Semaphore
+        from sim.Walker import Walker
+
+        # Core logic properties
+        self.matrix = matrix
+        self.cars: dict[UUID, Car] = {}
+        self.walkers: dict[UUID, Walker] = {}
         self.semaphores: dict[tuple[int, int], Semaphore] = {}
+        self.places : dict[tuple[int, int], PlaceBlock] = {}
+        self._extract_data() # Extract 
 
-        self._add_semaphores()
-        self._initialize()
-        # self.lock = Lock()
+        # Testing purpose call
+        self._initialize() 
 
-    def _add_semaphores(self) -> None:
+    def _extract_data(self) -> None:
         height = len(self.matrix)
         width = len(self.matrix[0])
 
         for i in range(height):
             for j in range(width):
                 block = self.matrix[i][j]
+
+                # Extract semaphores representatives
                 if isinstance(block, SemaphoreBlock) and block.representative not in self.semaphores:
                     from sim.Semaphore import Semaphore
                     self.semaphores[block.representative] = Semaphore(block.representative, self)
                     self.semaphores[block.representative].gui_label = len(self.semaphores)
 
+                # Extract interest places
+                if isinstance(block, PlaceBlock):
+                    self.places[(i, j)] = block
+
+    # Testing purpose method
     def _initialize(self):
         from sim.event_handler import EventHandler
         event_handler = EventHandler(self)
