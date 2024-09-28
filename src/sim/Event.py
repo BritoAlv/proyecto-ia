3cde29ab2ef10dd3f66a63079a912d48e0bf4cd7
Javier Lima <javi.limashelby@gmail.com>
2024-09-28 10:48:22 -0400

    feat: Get goal-positions according to interest places

--- src/sim/Event.py
+++ src/sim/Event.py
@@ -2,10 +2,11 @@ from datetime import datetime, timedelta
 from enum import Enum
 import math
 import random
+import numpy as np
 from numpy.random import exponential
 
 from environment import Environment, RoadBlock, SidewalkBlock
-from sim.Car import Car
+from sim.Car.Car import Car
 
 class EventType(Enum):
     CAR_EVENT = 0
@@ -40,16 +41,20 @@ class EventHandler:
     def _handle_car_event(self, event : Event) -> Event:
         # TODO: The exponential average time should vary depending on the time of the day (check on non-stationary Poisson Process to achieve accuracy)
         # Create a new Car-Event, using a Poisson distribution for car-event dates
-        time_offset = math.ceil(exponential(10))
+        time_offset = math.ceil(exponential(30))
         next_date = event.date + timedelta(seconds=time_offset)
         next_car_event = Event(next_date, EventType.CAR_EVENT)
 
         # Get non-occupied road-blocks
         free_blocks = self._get_free_blocks(RoadBlock)
-
         if len(free_blocks) > 0:
+            goals, goals_probabilities = self._get_roads_probabilities()
+
+            start_position = random.choice(free_blocks).coordinates
+            goal_position = random.choices(goals, goals_probabilities)[0]
+
             # Create and set up a new car
-            car = Car(random.choice(free_blocks).coordinates, random.choice(free_blocks).coordinates, self.environment, len(self.environment.cars))
+            car = Car(start_position, goal_position, self.environment, len(self.environment.cars))
             self.environment.cars[car.id] = car
 
         return next_car_event
@@ -85,3 +90,69 @@ class EventHandler:
                 if isinstance(block, block_type):
                     blocks.append(block)
         return blocks
+    
+    def _get_roads_probabilities(self) -> tuple[list[tuple[int, int]], list[float]]:
+        matrix = self.environment.matrix
+        height = len(matrix)
+        width = len(matrix[0])
+        places_probability : dict[tuple[int, int], float] = self._get_places_probability()
+
+        roads : list[tuple[int, int]] = []
+        probabilities : list[float] = []
+
+        for i in range(height):
+            for j in range(width):
+                block = matrix[i][j]
+                if isinstance(block, RoadBlock):
+                    roads.append(block.coordinates)
+                    if (i, j) in places_probability:
+                        probabilities.append(places_probability[(i, j)])
+                    else:
+                        probabilities.append(self._get_road_probability((i, j), places_probability))
+        
+        return roads, probabilities
+
+    
+    def _get_places_probability(self, car_biased : bool = True) -> dict[tuple[int, int], float]:
+        places_probabilities : dict[tuple[int, int], list[float]] = {}
+        places_probability : dict[tuple[int, int], float] = {}
+        
+        for place in self.environment.places.values():
+            # Process place
+            probability = 0.9 # Template, actual method should use nlp to calculate that probability
+
+            if place.representative not in places_probabilities:
+                places_probabilities[place.representative] = [probability]
+            else:
+                places_probabilities[place.representative].append(probability)
+        
+        for place_representative in places_probabilities:
+            places_probability[place_representative] = np.average(places_probabilities[place_representative])
+
+        return places_probability
+    
+    
+    def _get_manhattan_distance(self, coord1 : tuple[int, int], coord2 : tuple[int, int]):
+        row1, col1 = coord1
+        row2, col2 = coord2
+        return abs(row1 - row2) + abs(col1 - col2)
+    
+    
+    def _get_road_probability(self, road_coord : tuple[int, int], places_probability : dict[tuple[int, int], float]):
+        if len(places_probability) == 0:
+            return 0.5
+        
+        smallest_distance = 1e10
+        closest_place_probability = 0
+        for place_coord in places_probability:
+            place_probability = places_probability[place_coord]
+
+            distance = self._get_manhattan_distance(road_coord, place_coord)
+            if distance < smallest_distance:
+                smallest_distance = distance
+                closest_place_probability = place_probability
+
+        factor = 1 - 0.05 * smallest_distance if smallest_distance < 20 else 1 - 0.05 * 19
+
+        return closest_place_probability * factor
+ 
