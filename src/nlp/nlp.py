5a74a863d1affd7223acc0e99b632492af99d5f6
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2024-10-03 02:40:08 -0400

    fixed missing data

--- src/nlp/nlp.py
+++ src/nlp/nlp.py
@@ -19,17 +19,40 @@ class Nlp:
             "months": ["September", "October", "November", "December", "January", "February", "March", "April", "May", "June"],
             "hours": [7, 17]
         } 
-        entonces yo te voy a dar otro texto y quiero que me devuelvas un json como el que te enseñe y en la respuesta solo quiero ese json para el texto que te dare: '''
+        entonces yo te voy a dar otro texto y quiero que me devuelvas un json como el que te enseñe pero si algun de los campos no lo puedes obtener del texto no te inventes datos y en la respuesta solo quiero ese json para el texto que te dare: '''
 
     def response(self, text):
+        data = self.api_request(text)
+        if data.get('walkers'):
+            if data['walkers'] == None:
+                data['walkers'] = 0.3
+        else:
+            data['walkers'] = 0.3
+        if data.get('cars'):
+            if data['cars'] == None:
+                data['cars'] = 0.3
+        else:
+            data['cars'] = 0.3
+        if data.get('months'):
+            if data['months'] == None:
+                data['months'] = ["January, February"]
+        else:
+            data['months'] = ["January, February"]
+        if data.get('hours'):
+            if data['hours'] == None:
+                data['hours'] = [4, 6]
+        else:
+            data['hours'] = [4, 6]
+        return data
+    
+    def api_request(self, text):
         response = self.llm.chat.complete(
-        model = "mistral-large-latest",
-        messages = [
+            model = "mistral-large-latest",
+            messages = [
                 {"role": "user", "content": self.initial_prompt + text}
             ]
         )
         return self.convert_json(response.choices[0].message.content)
-    
     def convert_json(self, text):
         cleaned_json_string = text[8:-3]
         try:
@@ -39,3 +62,5 @@ class Nlp:
         except json.JSONDecodeError:
             return None
 
+nlp = Nlp()
+print(nlp.response("Gran lugar, abierto de cinco a seis de la madrugada, un mes si otro no en los meses pares o sea enero febrero marzo mayo"))
